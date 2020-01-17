a90e1c89138570777e72c08d643932640e12842c
Olivier Goffart <ogoffart@woboq.com>
2020-01-17 14:02:26 +0100

    blinking led

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,34 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
+use stm32f4::stm32f412;
+use stm32f4xx_hal::{stm32, prelude::*};
+
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let gpiob = dp.GPIOB.split();
+    let gpioc = dp.GPIOC.split();
+    let gpioa = dp.GPIOA.split();
+
+    let mut led_wifi = gpiob.pb2.into_push_pull_output();
+    let mut led_user = gpioc.pc13.into_push_pull_output();
+    let mut led_azure = gpioa.pa15.into_push_pull_output();
+
+    let mut led_r = gpiob.pb4.into_push_pull_output();
+    let mut led_b = gpioc.pc7.into_push_pull_output();
+    let mut led_g = gpiob.pb3.into_push_pull_output();
+
+    let btn_a = gpioa.pa4.into_pull_up_input();
+    let btn_b = gpioa.pa10.into_pull_up_input();
 
     loop {
-        // your code goes here
+
+        led_b.set_high();
+        cortex_m::asm::delay(2000000);
+        led_b.set_low();
+        cortex_m::asm::delay(2000000);
     }
 }
